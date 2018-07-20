b2ccf9ba69ddd006930e7d32ccfc1618e549d0b0
xiongke <>
2018-07-20 10:58:08 +0800

    upgrade  validator  v8 to v9 and translate errors

--- binding/default_validator.go
+++ binding/default_validator.go
@@ -8,29 +8,41 @@ import (
 	"reflect"
 	"sync"
 
-	"gopkg.in/go-playground/validator.v8"
+	"gopkg.in/go-playground/validator.v9"
+	"github.com/go-playground/locales/zh"
+	"github.com/go-playground/universal-translator"
+	zh_translations "gopkg.in/go-playground/validator.v9/translations/zh"
+	"fmt"
+	"errors"
 )
 
 type defaultValidator struct {
 	once     sync.Once
 	validate *validator.Validate
+	trans ut.Translator
 }
 
 var _ StructValidator = &defaultValidator{}
 
 // ValidateStruct receives any kind of type, but only performed struct or pointer to struct type.
 func (v *defaultValidator) ValidateStruct(obj interface{}) error {
-	value := reflect.ValueOf(obj)
-	valueType := value.Kind()
-	if valueType == reflect.Ptr {
-		valueType = value.Elem().Kind()
-	}
-	if valueType == reflect.Struct {
+
+	if kindOfData(obj) == reflect.Struct {
+
 		v.lazyinit()
+
 		if err := v.validate.Struct(obj); err != nil {
-			return err
+			//return err
+			errs := err.(validator.ValidationErrors)
+			errstring := ""
+			for _, e := range errs {
+				// can translate each error one at a time.
+				errstring += fmt.Sprintf("%s", e.Translate(v.trans))
+			}
+			return errors.New(errstring)
 		}
 	}
+
 	return nil
 }
 
@@ -45,7 +57,25 @@ func (v *defaultValidator) Engine() interface{} {
 
 func (v *defaultValidator) lazyinit() {
 	v.once.Do(func() {
-		config := &validator.Config{TagName: "binding"}
-		v.validate = validator.New(config)
+		zh := zh.New()
+		uni := ut.New(zh, zh)
+		v.trans ,_ = uni.GetTranslator("zh")
+
+		v.validate = validator.New()
+		v.validate.SetTagName("binding")
+
+		zh_translations.RegisterDefaultTranslations(v.validate, v.trans)
 	})
+}
+
+
+func kindOfData(data interface{}) reflect.Kind {
+
+	value := reflect.ValueOf(data)
+	valueType := value.Kind()
+
+	if valueType == reflect.Ptr {
+		valueType = value.Elem().Kind()
+	}
+	return valueType
 }
